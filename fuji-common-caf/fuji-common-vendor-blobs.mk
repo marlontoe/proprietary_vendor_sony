e77cee2642cd7533bb790d2b14fe621bf78ac744
RaymanFX <raymanfx@gmail.com>
2014-01-01 22:42:59 +0100

    fuji-caf: import HTC ONE GE 4.4.2 blobs

--- fuji-common-caf/fuji-common-vendor-blobs.mk
+++ fuji-common-caf/fuji-common-vendor-blobs.mk
@@ -23,13 +23,13 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/proprietary/system/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
     $(COMMON_PATH)/proprietary/system/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
     $(COMMON_PATH)/proprietary/system/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
+    $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv2_adreno.so:system/lib/egl/libGLESv2_adreno200.so \
     $(COMMON_PATH)/proprietary/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv2S3D_adreno200.so:system/lib/egl/libGLESv2S3D_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libplayback_adreno200.so:system/lib/egl/libplayback_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv2S3D_adreno.so:system/lib/egl/libGLESv2S3D_adreno200.so \
+    $(COMMON_PATH)/proprietary/system/lib/egl/libq3dtools_adreno.so:system/lib/egl/libq3dtools_adreno200.so \
+    $(COMMON_PATH)/proprietary/system/lib/egl/libEGL_adreno.so:system/lib/egl/libEGL_adreno200.so \
+    $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv1_CM_adreno.so:system/lib/egl/libGLESv1_CM_adreno200.so \
+    $(COMMON_PATH)/proprietary/system/lib/libadreno_utils.so:system/lib/libadreno_utils.so \
     $(COMMON_PATH)/proprietary/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     $(COMMON_PATH)/proprietary/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
     $(COMMON_PATH)/proprietary/system/lib/libOpenCL.so:system/lib/libOpenCL.so \
