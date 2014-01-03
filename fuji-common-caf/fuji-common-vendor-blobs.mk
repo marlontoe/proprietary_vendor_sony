70d25cfd188f9fb6f81f59ad8be8728159655f4a
RaymanFX <raymanfx@gmail.com>
2014-01-03 13:17:23 +0100

    fuji-caf: move HTC ONE GE 4.4.2 blobs

--- fuji-common-caf/fuji-common-vendor-blobs.mk
+++ fuji-common-caf/fuji-common-vendor-blobs.mk
@@ -23,19 +23,19 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/proprietary/system/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
     $(COMMON_PATH)/proprietary/system/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
     $(COMMON_PATH)/proprietary/system/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv2_adreno.so:system/lib/egl/libGLESv2_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv2S3D_adreno.so:system/lib/egl/libGLESv2S3D_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libq3dtools_adreno.so:system/lib/egl/libq3dtools_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libEGL_adreno.so:system/lib/egl/libEGL_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/egl/libGLESv1_CM_adreno.so:system/lib/egl/libGLESv1_CM_adreno200.so \
-    $(COMMON_PATH)/proprietary/system/lib/libadreno_utils.so:system/lib/libadreno_utils.so \
-    $(COMMON_PATH)/proprietary/system/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
-    $(COMMON_PATH)/proprietary/system/lib/libsc-a2xx.so:system/lib/libsc-a2xx.so \
-    $(COMMON_PATH)/proprietary/system/lib/libOpenCL.so:system/lib/libOpenCL.so \
-    $(COMMON_PATH)/proprietary/system/lib/libOpenVG.so:system/lib/libOpenVG.so \
-    $(COMMON_PATH)/proprietary/system/lib/libC2D2.so:system/lib/libC2D2.so \
-    $(COMMON_PATH)/proprietary/system/lib/libgsl.so:system/lib/libgsl.so
+    $(COMMON_PATH)/proprietary/vendor/lib/egl/libGLESv2_adreno.so:vendor/lib/egl/libGLESv2_adreno200.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/egl/eglsubAndroid.so:vendor/lib/egl/eglsubAndroid.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/egl/libGLESv2S3D_adreno.so:vendor/lib/egl/libGLESv2S3D_adreno200.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/egl/libq3dtools_adreno.so:vendor/lib/egl/libq3dtools_adreno200.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/egl/libEGL_adreno.so:vendor/lib/egl/libEGL_adreno200.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:vendor/lib/egl/libGLESv1_CM_adreno200.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/libadreno_utils.so:vendor/lib/libadreno_utils.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/libc2d2_z180.so:vendor/lib/libc2d2_z180.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/libsc-a2xx.so:vendor/lib/libsc-a2xx.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/libOpenCL.so:vendor/lib/libOpenCL.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/libOpenVG.so:vendor/lib/libOpenVG.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/libC2D2.so:vendor/lib/libC2D2.so \
+    $(COMMON_PATH)/proprietary/vendor/lib/libgsl.so:vendor/lib/libgsl.so
 
 # OMX
 PRODUCT_COPY_FILES += \
