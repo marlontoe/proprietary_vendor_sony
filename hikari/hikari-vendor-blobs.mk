215489ad14e545adf7d3fc44fd161e48161c2b26
Warren <deed821@gmail.com>
2014-07-20 21:02:41 +1000

    add vendor files for time fix
    Change-Id: Ibb9b95860133dbb27e4066e330780b982f653208

--- hikari/hikari-vendor-blobs.mk
+++ hikari/hikari-vendor-blobs.mk
@@ -160,6 +160,8 @@ PRODUCT_COPY_FILES += \
     vendor/sony/hikari/proprietary/lib/libta_rmt_clnt.so:system/lib/libta_rmt_clnt.so \
     vendor/sony/hikari/proprietary/lib/libthermal_mitigation.so:system/lib/libthermal_mitigation.so \
     vendor/sony/hikari/proprietary/lib/libthermal_mitigation_fusion.so:system/lib/libthermal_mitigation_fusion.so \
+    vendor/sony/hikari/proprietary/lib/libtime_genoff.so:obj/lib/libtime_genoff.so \
+	vendor/sony/hikari/proprietary/lib/libtime_genoff.so:system/lib/libtime_genoff.so \
     vendor/sony/hikari/proprietary/vendor/camera/APT01BM0.dat:system/vendor/camera/APT01BM0.dat \
     vendor/sony/hikari/proprietary/vendor/camera/KMO13BS0.dat:system/vendor/camera/KMO13BS0.dat \
     vendor/sony/hikari/proprietary/vendor/camera/KMO13BS0_BU6456.dat:system/vendor/camera/KMO13BS0_BU6456.dat \
--- nozomi/nozomi-vendor-blobs.mk
+++ nozomi/nozomi-vendor-blobs.mk
@@ -163,6 +163,8 @@ PRODUCT_COPY_FILES += \
     vendor/sony/nozomi/proprietary/lib/libta_rmt_clnt.so:system/lib/libta_rmt_clnt.so \
     vendor/sony/nozomi/proprietary/lib/libthermal_mitigation.so:system/lib/libthermal_mitigation.so \
     vendor/sony/nozomi/proprietary/lib/libthermal_mitigation_fusion.so:system/lib/libthermal_mitigation_fusion.so \
+    vendor/sony/nozomi/proprietary/lib/libtime_genoff.so:obj/lib/libtime_genoff.so \
+	vendor/sony/nozomi/proprietary/lib/libtime_genoff.so:system/lib/libtime_genoff.so \
     vendor/sony/nozomi/proprietary/vendor/camera/APT01BM0.dat:system/vendor/camera/APT01BM0.dat \
     vendor/sony/nozomi/proprietary/vendor/camera/KMO13BS0.dat:system/vendor/camera/KMO13BS0.dat \
     vendor/sony/nozomi/proprietary/vendor/camera/KMO13BS0_BU6456.dat:system/vendor/camera/KMO13BS0_BU6456.dat \
